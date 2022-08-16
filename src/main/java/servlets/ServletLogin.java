089884926bf9e4e3749781b97ebeba3c333618e2
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-08-16 16:42:31 -0300

    criação da classe Filter

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,10 +29,38 @@ public class ServletLogin extends HttpServlet {
 			throws ServletException, IOException {
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
-		ModelLogin modelLogin = new ModelLogin();
-		modelLogin.setLogin(login);
-		modelLogin.setSenha(senha);
-
+		String url = request.getParameter("url");
+		
+		
+		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+			
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+			
+			if (modelLogin.getLogin().equals("admin") 
+					&& modelLogin.getSenha().equals("admin")) {/*Deu certo o  login*/
+				
+				request.getSession().setAttribute("usuario", modelLogin);/*Coloca o user na sessao*/
+				
+				if (url == null || url.equals("null")) {
+					url = "principal/principal.jsp";
+				}
+				RequestDispatcher redirecionar = request.getRequestDispatcher(url);
+				redirecionar.forward(request, response);
+				
+			}else {
+				RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
+				request.setAttribute("msg", "Informe o login e senha corretamente!");
+				redirecionar.forward(request, response);
+			}
+			
+		}else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Informe o login e senha corretamente!");
+			redirecionar.forward(request, response);
+		}
+		
 	}
 
 }
